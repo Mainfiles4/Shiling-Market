715aba5e9415d2b7139fcfe7c327fa36aa56a2f8
Mainfiles4 <filesmain4@gmail.com>
2024-03-31 18:20:50 +0300

    CORS policy setted

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,10 +1,19 @@
 <?php
 
+// Set CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Role, Origin, Content-Type, Authorization"); // Add Authorization header
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit; // Preflight request, exit early
+}
+
 $routes->get('/', 'ApiControllers::index');
 $routes->post('/', 'ApiControllers::index');
 $routes->get('/api/v1', 'ApiControllers::index');
@@ -14,7 +23,7 @@ $routes->post('/api/v1', 'ApiControllers::index');
 $routes->post('api/v1/auth-register', 'AuthController::register');
 $routes->post('api/v1/auth-login', 'AuthController::login');
 
-// for products manpulation
+// for products manipulation
 $routes->get('/api/v1/products', 'ProductsController::index');
 $routes->post('/api/v1/products', 'ProductsController::create');
 
